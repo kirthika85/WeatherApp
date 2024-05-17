f1582ce986d492f141b4c0b626739873422208ee
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-17 12:14:32 -0700

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -4,23 +4,16 @@ from pyowm import OWM
 owm = OWM('47d570067856f5d716bbea83635e8c26')
 mgr = owm.weather_manager()
 
-# Streamlit app
-st.title('Weather Prediction App')
-
-# Input fields for city and state
+st.title('Weather Prediction App US')
 city = st.text_input('Enter the city name')
 state = st.text_input('Enter the state name')
 
-# Button to predict weather
 if st.button('Predict Weather'):
     if city and state:
         location = f'{city},{state},US'
         try:
-            # Get weather forecast
-            observation = mgr.weather_at_place(location)
-            weather = observation.weather
-
-            # Display weather forecast
+            input = mgr.weather_at_place(location)
+            weather = input.weather
             st.write(f'Weather forecast for {city}, {state}:')
             st.write(f'Temperature: {weather.temperature("fahrenheit")["temp"]} °F')
             st.write(f'Humidity: {weather.humidity} %')
